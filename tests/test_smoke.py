008c16fd9afe054bbc063417ce85cb767ac30f6e
Blake Torres <blaketorres2000@gmail.com>
2024-06-18 11:48:59 -0500

    renamed smoke test to test_smoke

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
+
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless=new")
